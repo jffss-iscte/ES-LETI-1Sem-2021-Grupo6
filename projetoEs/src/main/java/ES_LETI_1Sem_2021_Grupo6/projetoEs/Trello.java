5ab92e8ef7ee3e43749fd1ba86095914f3f61f38
Manuel Casimiro <mrcos1@iscte-iul.pt>
2021-11-16 11:42:53 +0000

    Código sem erros

--- projetoEs/src/main/java/ES_LETI_1Sem_2021_Grupo6/projetoEs/Trello.java
+++ projetoEs/src/main/java/ES_LETI_1Sem_2021_Grupo6/projetoEs/Trello.java
@@ -1,7 +1,28 @@
 package ES_LETI_1Sem_2021_Grupo6.projetoEs;
 
+import java.util.List;
+
+import com.julienvey.trello.impl.TrelloImpl;
+import com.julienvey.trello.impl.http.ApacheHttpClient;
+import com.julienvey.trello.domain.Board;
+import com.julienvey.trello.domain.TList;
+
 public class Trello {
 
-	
-	
+	String key= "0c5239eff21038d1191eb11906e8f0ec";
+	String trelloAccessToken="b00fb2a4469ff1a76559af3db24eee2720b98429df59b0ffd4f7d34127c0cd1f";
+	TrelloImpl trelloApi = new TrelloImpl(key, trelloAccessToken, new ApacheHttpClient());
+
+	Board board;
+	List<Board> member= trelloApi.getMemberBoards("mrcosiscte");{ 
+		for (Board quadro : member){             
+			System.out.println(quadro.getName()+ "-" +quadro.getId());
+			board = trelloApi.getBoard(quadro.getId());
+			List<TList> lists = board.fetchLists();
+			for (TList lista : lists) {
+				System.out.println(lista.getName()+"- "+ lista.getId()+"-"+lista.getIdBoard());   
+			}
+		}
+	}
 }
+
