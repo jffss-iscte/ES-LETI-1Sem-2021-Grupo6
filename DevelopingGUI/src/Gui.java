7e3df9f1fadf4cc017358329be5e195b66e9d7f4
franr <franr@LAPTOP-C2F1F8GN>
2021-12-01 18:37:43 +0000

    update

--- DevelopingGUI/src/Gui.java
+++ DevelopingGUI/src/Gui.java
@@ -6,7 +6,7 @@ public class Gui {
 	      public static void main(String args[]){
 	        JFrame frame = new JFrame("My First GUI");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setSize(300,300);
+	     https://marketplace.eclipse.org/marketplace-client-intro?mpc_install=4008412   frame.setSize(300,300);
 	       JButton button1 = new JButton("Press");
 	       frame.getContentPane().add(button1);
 	       frame.setVisible(true);
